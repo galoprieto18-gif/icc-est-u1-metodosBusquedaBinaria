01d376cf8f6b2ce547b579acafba53105225a573
Galito-18 <238720115+galoprieto18-gif@users.noreply.github.com>
2026-05-18 08:34:39 -0500

    busquedaB

---
+++ src/BusquedaBinaria.java
@@ -0,0 +1,54 @@
+import Models.Product;
+
+public class BusquedaBinaria {
+
+    public void sortByName (Product [] products){
+        for (int i = 0; i< products.length-1; i++){
+            boolean swap = false;
+            for (int j =0; j<products.length-i-1; j++){
+                if(products[j].getNombre().compareTo(products[j+1].getNombre())>0){
+                    Product temp = products[j];
+                    products[j] = products[j+1];
+                    products[j+1] = temp;
+                    swap = true; 
+                }
+            }
+            if (!swap){
+                break;
+            }
+
+        }
+    }
+
+    public int findByName(Product [] products, String nombre){
+        // definamos bajo & alto 
+        int bajo = 0;
+        int alto = products.length - 1;
+         // comparemos con la clave 
+        while (bajo <= alto){
+            int central = (bajo + alto) / 2;
+            if(products [central].getNombre().equals(nombre)) {
+                return central;
+            }
+            // Si voy a la der o izq
+            int comparacion = products[central].getNombre().compareTo(nombre);
+            if(comparacion < 0) {
+                bajo = central + 1 ;
+            } else{
+                alto = central - 1 ;
+            }
+    return -1;
+        }
+
+        // definamos bajo & alto 
+        // comparemos con la clave 
+        // si es igual, retornamos el punto medio 
+        // si es mayor, actualizamos el index correspondiente
+        // si es menor, actualizamos el index correspondiente 
+
+
+    }
+    
+
+    
+}
---
+++ src/Models/Product.java
@@ -0,0 +1,46 @@
+package Models;
+
+public class Product {
+    //2 atributos
+    //nombre
+    //cantidad
+    //constructor con los atributos 
+    //getters & setters 
+    //toString 
+
+    private String nombre;
+    private int cantidad;
+    
+
+    
+    public Product() {
+    }
+
+    public Product(String nombre, int cantidad) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto [nombre=" + nombre + ", cantidad=" + cantidad + "]";
+    }
+
+    
+}
